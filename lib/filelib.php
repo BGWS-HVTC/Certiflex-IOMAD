79164397b66c064605fd8a8dd336bea8757b3fe2
Mike Robb <mrobb@bgworkforce.com>
2025-05-08 15:58:26 -0400

    fixing filelib's source unserialization

--- lib/filelib.php
+++ lib/filelib.php
@@ -911,7 +911,11 @@ function file_get_submitted_draft_itemid($elname) {
  * @return stored_file
  */
 function file_restore_source_field_from_draft_file($storedfile) {
-    $source = @unserialize($storedfile->get_source() ?? '');
+    // BGWS Modification START
+    // Author - Mike Robb
+    // Jira ticket - CER-56
+    $source = @json_decode($storedfile->get_source() ?? '');
+    // BGWS Modification END
     if (!empty($source)) {
         if (is_object($source)) {
             $restoredsource = $source->source;
@@ -1205,7 +1209,11 @@ function file_save_draft_area_files($draftitemid, $contextid, $component, $filea
             // Let's check if we can update this file or we need to delete and create.
             if ($newfile->is_directory()) {
                 // Directories are always ok to just update.
-            } else if (($source = @unserialize($newfile->get_source() ?? '')) && isset($source->original)) {
+            // BGWS Modification START
+            // Author - Mike Robb
+            // Jira ticket - CER-56
+            } else if (($source = @json_decode($newfile->get_source() ?? '')) && isset($source->original)) {
+            // BGWS Modification END
                 // File has the 'original' - we need to update the file (it may even have not been changed at all).
                 $original = file_storage::unpack_reference($source->original);
                 if ($original['filename'] !== $oldfile->get_filename() || $original['filepath'] !== $oldfile->get_filepath()) {
@@ -1241,7 +1249,11 @@ function file_save_draft_area_files($draftitemid, $contextid, $component, $filea
             // Field files.source for draftarea files contains serialised object with source and original information.
             // We only store the source part of it for non-draft file area.
             $newsource = $newfile->get_source();
-            if ($source = @unserialize($newfile->get_source() ?? '')) {
+            // BGWS Modification START
+            // Author - Mike Robb
+            // Jira ticket - CER-
+            if ($source = @json_decode($newfile->get_source() ?? '')) {
+            // BGWS Modification END
                 $newsource = $source->source;
             }
             if ($oldfile->get_source() !== $newsource) {
@@ -1275,7 +1287,11 @@ function file_save_draft_area_files($draftitemid, $contextid, $component, $filea
         // the size and subdirectory tests are extra safety only, the UI should prevent it
         foreach ($newhashes as $file) {
             $file_record = array('contextid'=>$contextid, 'component'=>$component, 'filearea'=>$filearea, 'itemid'=>$itemid, 'timemodified'=>time());
-            if ($source = @unserialize($file->get_source() ?? '')) {
+            // BGWS Modification START
+            // Author - Mike Robb
+            // Jira ticket - CER-56
+            if ($source = @json_decode($file->get_source() ?? '')) {
+            // BGWS Modification END
                 // Field files.source for draftarea files contains serialised object with source and original information.
                 // We only store the source part of it for non-draft file area.
                 $file_record['source'] = $source->source;
@@ -2933,7 +2949,11 @@ function file_overwrite_existing_draftfile(stored_file $newfile, stored_file $ex
 
     $fs = get_file_storage();
     // Remember original file source field.
-    $source = @unserialize($existingfile->get_source() ?? '');
+    // BGWS Modification START
+    // Author - Mike Robb
+    // Jira ticket - CER-56
+    $source = @json_decode($existingfile->get_source() ?? '');
+    // BGWS Modification END
     // Remember the original sortorder.
     $sortorder = $existingfile->get_sortorder();
     if ($newfile->is_external_file()) {
@@ -2957,7 +2977,11 @@ function file_overwrite_existing_draftfile(stored_file $newfile, stored_file $ex
     $newfile = $fs->create_file_from_storedfile($newfilerecord, $newfile);
     // Preserve original file location (stored in source field) for handling references.
     if (isset($source->original)) {
-        if (!($newfilesource = @unserialize($newfile->get_source() ?? ''))) {
+        // BGWS Modification START
+        // Author - Mike Robb
+        // Jira ticket - CER-56
+        if (!($newfilesource = @json_decode($newfile->get_source() ?? ''))) {
+        // BGWS Modification END
             $newfilesource = new stdClass();
         }
         $newfilesource->original = $source->original;
